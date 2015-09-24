237ca9d889c2b3bd796d6f3905332013aa4b12a8
randgalt <jordan@jordanzimmerman.com>
2015-09-24 00:11:55 -0500

    initial commit

---
+++ js/jz.js
@@ -0,0 +1,63 @@
+function item(url, image, imageWidth, imageHeight, alt, special) {
+    this.url = url;
+    this.image = image;
+    this.imageWidth = imageWidth;
+    this.imageHeight = imageHeight;
+    this.alt = alt;
+    this.special = special;
+}
+
+var items = [
+    new item('https://www.facebook.com/jordan.zimmerman', 'FB-f-Logo__blue_144.png', 144, 144, 'Facebook'),
+    new item('https://www.linkedin.com/in/jordanzimmerman', 'Logo-2C-41px-TM.png', 170, 41, 'LinkedIn'),
+    new item('https://github.com/randgalt', 'GitHub_Logo.png', 160, 66, 'GitHub'),
+    new item('https://www.twitter.com/randgalt', 'TwitterLogo_55acee.png', 166, 166, 'Twitter'),
+    new item('https://www.facebook.com/ritahkan', 'rita.png', 101, 136, 'Rita Kan'),
+    new item('https://www.facebook.com/bobby.zimmerman.716', 'doodles.jpg', 170, 128, 'Bobby Doodle'),
+    new item('https://soundcloud.com/jordanzimmerman', 'sc_st_gradient_152x96.png', 152, 96, 'SoundCloud'),
+    new item('http://aisallc.com', 'aisascaled.png', 165, 147, 'Aisa Software'),
+    new item(null, null, 0, 0, null, 'mail'),
+    new item('http://curator.apache.org', 'curator.png', 150, 56, 'Apache Curator'),
+    new item('http://nirmataoss.github.io/workflow', 'nirmata.jpg', 100, 100, 'Nirmata Workflow'),
+    new item('http://soabase.io', 'soabase.png', 125, 125, 'Soabase'),
+    new item('http://www.google.com/patents/US7197475', 'patent.png', 125, 156, 'US Patent 7197475'),
+    new item('olivegarden.html', 'olivegarden.jpg', 151, 115, 'Olive Garden Dish Name Generator'),
+];
+
+var ITEMS_PER_ROW = 4;
+var ROW_CLASS = 'col-md-' + (12 / ITEMS_PER_ROW);
+
+function buildItems() {
+    var content = '';
+    for ( var i in items ) {
+        var index = parseInt(i);
+        var item = items[i];
+        if ( (index % ITEMS_PER_ROW) == 0 ) {
+            content = content + '<div class="row">\n';
+        }
+
+        content = content + '<div class="' + ROW_CLASS + '">\n';
+        if ( item.special === 'mail' ) {
+            content = content + '<a id="jzenvelope-a" href="#" class="jzthumb thumbnail"><span id="jzenvelope" class="glyphicon glyphicon-envelope"></span></a>\n';
+        } else {
+            content = content + '<a href="' + item.url + '" class="jzthumb thumbnail"><img src="assets/' + item.image + '" width="' + item.imageWidth + '" height="' + item.imageHeight + '" alt="' + item.alt + '"></a>\n';
+        }
+        content = content + '</div>\n';
+
+        if ( (((index + 1) % ITEMS_PER_ROW) == 0) || ((index + 1) == items.length))  {
+            content = content + '</div>\n';
+        }
+    }
+    $('#items').html(content);
+}
+
+$(function(){
+    buildItems();
+
+    var m2 = 'mail';
+    $('#copyright').text(new Date().getFullYear());
+    m2 = m2 + 'to:';
+    m2 = m2 + 'jordan';
+    m2 = m2 + '@jordanzimmerman.com';
+    $('#jzenvelope-a').attr('href', m2);    
+});
